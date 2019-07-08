ce877be2eef06c1f9d0d18c9e5aceb1d5db47f83
daniel.abayomi <daniel.abayomi@arkounting.com.ng>
2019-07-08 13:37:08 +0100

    New commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import Person from './Person/Person';
+
+class App extends Component{
+state = {
+  persons: [
+    {id: "lnd001", name: "Daniel", age: 25},
+    {id: "lnd002", name: "Joshua", age: 26},
+    {id: "lnd003", name: "Habib", age: 27}
+  ],
+  showPersons: false
+}
+
+nameChangedHandler = (event, id) => {
+  const personIndex = this.state.persons.findIndex(p => {
+    return p.id === id;
+  });
+
+  const person = {...this.state.persons[personIndex]};
+  person.name = event.target.value;
+
+  const persons = [...this.state.persons];
+  persons[personIndex] = person;
+
+  this.setState({persons: persons})
+}
+
+deletePersonHandler = (personIndex) => {
+  const persons = [...this.state.persons];
+  persons.splice(personIndex, 1);
+  this.setState({persons: persons})
+}
+
+togglePersonsHandler = () => {
+  const doesShow = this.state.showPersons;
+  this.setState({
+    showPersons: !doesShow
+  })
+}
+render() {
+  const style = {
+    backgroundColor: 'cadetblue',
+    // border: '1px solid cadetblue',
+    font: 'inherit',
+    padding: '8px',
+    cursor: 'pointer',
+    color: '#ffffff',
+    borderRadius: '4px',
+  }
+
+  let persons = null;
+
+  if(this.state.showPersons) {
+    persons = (
+      <div>
+      {this.state.persons.map((person, index) => {
+        return <Person 
+        click={() => this.deletePersonHandler(index)}
+        name={person.name} 
+        age={person.age}
+        key={person.id}
+        changed={(event) => this.nameChangedHandler(event, person.id)}
+        />
+      })}
+        </div>
+    );
+
+    style.backgroundColor = "red";
+  }
+
+  const classes = [];
+  if(this.state.persons.length <= 2) {
+    classes.push('red');
+  } if (this.state.persons.length <= 1) {
+    classes.push('bold');
+  }
+  return (
+    <div className="App">
+      <h1>Test Header</h1>
+      <p className={classes.join(' ')}>This is a test paragraph</p>
+      <button 
+        style={style}
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
+  );
+}  
+}
+
+export default App;
